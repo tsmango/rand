e1fcc92306bf3b47fbd869838cd903f563214840
Thomas Mango <tsmango@gmail.com>
2009-07-02 15:26:38 -0400

    Initial commit.

---
+++ lib/rand.rb
@@ -0,0 +1,16 @@
+class Array
+  def rand(size = 1)
+    return nil if size < 1
+    
+    if size == 1
+      return self[Kernel.rand(self.length)]
+    else
+      random_candidates = self.clone
+      size = self.length if size > self.length
+      
+      (0..(size - 1)).to_a.collect do
+        random_candidates.delete_at(Kernel.rand(random_candidates.length))
+      end
+    end
+  end
+end
